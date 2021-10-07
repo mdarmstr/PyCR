b4ec99ce0f8d710b616ae8bf1d7efd8741276a77
Rikyyyyyyyyyy <43457622+Rikyyyyyyyyyy@users.noreply.github.com>
2021-10-07 20:03:37 +0800

    update

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 import xlrd
 import ClustResolution
+import setClass
 def main():
-    # To open Workbook
-    wb = xlrd.open_workbook('data/CasevControl.xlsx')
-    # select the first sheet from xlsx file
-    sheet = wb.sheet_by_index(0)
-    # print(sheet.cell_value(0, 0))
-    # print(sheet.nrows)
-    # define 2 arrays
-    clust1 = []
-    clust2 = []
-    # For loop compare with each group [1 or 2] and put different datda in different clust array
-    for i in range(1, sheet.nrows):
-        if int(sheet.cell_value(i, 2)) == 1:
-            clust1.append([float(sheet.cell_value(i, 0)), float(sheet.cell_value(i, 1))])
+    setClass.setClass('data/CasevControl.xlsx')
 
-        elif int(sheet.cell_value(i, 2)) == 2:
-            clust2.append([float(sheet.cell_value(i, 0)), float(sheet.cell_value(i, 1))])
-    # Call function ClustResilution to do further calculation
-    ClustResolution.clustResolution(clust1, clust2)
 main()
--- setClass.py
+++ setClass.py
@@ -1,2 +1,18 @@
+import xlrd
 def setClass(fileName):
-    
+    wb = xlrd.open_workbook('data/CasevControl.xlsx')
+    # select the first sheet from xlsx file
+    sheet = wb.sheet_by_index(0)
+    #define a class dictionary for later use
+    class_dict = {}
+    class_value_counter = 1
+    # save the first class item into our class dictionary
+    class_dict[sheet.cell_value(1, 1)] = class_value_counter
+    class_value_counter = class_value_counter + 1
+    # check all the item and add new class into class dictionary
+    for i in range(1, sheet.nrows):
+        if sheet.cell_value(i,1) in class_dict.keys():
+            pass
+        else:
+            class_dict[sheet.cell_value(i, 1)] = class_value_counter
+            class_value_counter = class_value_counter + 1
